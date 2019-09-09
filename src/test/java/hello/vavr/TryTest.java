0f37fafe04276bf0397de651131c27930e507fe1
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2019-09-09 08:27:43 +0200

    update Try sample.

--- src/test/java/hello/vavr/TryTest.java
+++ src/test/java/hello/vavr/TryTest.java
@@ -2,15 +2,12 @@ package hello.vavr;
 
 import io.vavr.Function1;
 import io.vavr.Function2;
-import io.vavr.collection.List;
 import io.vavr.control.Try;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
+import static io.vavr.control.Try.success;
 import static java.lang.Integer.MAX_VALUE;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,7 +19,7 @@ public class TryTest {
         // Function2<Integer, Integer, Try<Integer>> tryDivide = (x, y) -> Try.of(() -> unsafeDivide.apply(x, y));
         var tryDivide = Function2.liftTry(unsafeDivide);
 
-        assertThat(tryDivide.apply(10, 2)).isEqualTo(Try.success(5));
+        assertThat(tryDivide.apply(10, 2)).isEqualTo(success(5));
 
         var failingDivision = tryDivide.apply(10, 0);
         assertThat(failingDivision.isFailure()).isTrue();
@@ -32,22 +29,47 @@ public class TryTest {
     }
 
     @Test
-    public void tryToReadFiles() {
-        Function1<String, Try<List<String>>> tryReadLines = filename -> Try.of(() -> readLinesFrom(filename));
+    public void tryCallService() {
+        Function1<String, Try<Response>> tryCall = value -> Try.of(() -> callService(value));
 
-        var result = tryReadLines.apply("/first.txt")
-                .mapTry(lines -> lines.get(1))
-                .flatMapTry(tryReadLines::apply)
-                .mapTry(lines -> lines.get(1))
-                .onFailure(Throwable::printStackTrace);
+        Try<Response> result1 = tryCall.apply(null);
+        assertThat(result1.isFailure()).isTrue();
+        System.out.println("Result 1: " + result1);
 
-        assertThat(result.getOrElse("FAILED")).isEqualTo("SUCCESS");
+        Try<Response> result2 = tryCall.apply("INVALID");
+        assertThat(result2.isSuccess()).isTrue();
+        System.out.println("Result 2: " + result2.get());
+
+        Try<Response> result3 = result2.flatMap(response -> tryCall.apply(response.value));
+        assertThat(result3.isFailure()).isTrue();
+        System.out.println("Result 3: " + result3);
+
+        Try<Response> result4 = tryCall.apply("valid");
+        assertThat(result4.isSuccess()).isTrue();
+        System.out.println("Result 4: " + result4.get());
+
+        Try<String> mappedResult4 = result4.map(response -> response.value.substring(0, 1));
+        assertThat(mappedResult4).isEqualTo(success("v"));
+
+        Try<Response> result5 = result4.flatMap(response -> tryCall.apply(response.value));
+        assertThat(result5.isSuccess()).isTrue();
+        System.out.println("Result 5: " + result5.get());
     }
 
-    private List<String> readLinesFrom(String filename) throws Exception {
-        var resource = this.getClass().getResource(filename);
-        if (resource == null) throw new IOException("Resource not found: " + filename);
-        return List.ofAll(Files.readAllLines(Paths.get(resource.toURI()), Charset.defaultCharset()));
+    private Response callService(String value) throws IOException {
+        if (value == null) throw new IOException();
+        return value.equals("INVALID") ? new Response(null) : new Response(value);
+    }
+
+    private static class Response {
+        private final String value;
+        private Response(String value) {
+            this.value = value == null ? null : value + "'";
+        }
+        @Override
+        public String toString() {
+            return value;
+        }
     }
 
 }
